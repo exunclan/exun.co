af7314d466d55a3fe5be18828bea073bf2527b39
Angad Singh <mail@angad.dev>
2020-07-10 11:38:49 +0530

    Render 404 template in error handler instead of * route

--- routes/index.js
+++ routes/index.js
@@ -79,8 +79,7 @@ router.get('*', async ctx => {
   }
   const shortlink = await Shortlink.query().findOne('slug', pathname.slice(1))
   if (!shortlink) {
-    await ctx.render('404')
-    ctx.status = 404
+    ctx.throw(404)
     return
   }
   ctx.redirect(shortlink.target)
